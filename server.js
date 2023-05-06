22de081557945e0ecc66864115c5090843d563b6
Jacob Spackman <jacobts202@gmail.com>
2023-05-06 09:25:54 -0600

    server work

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express()
 app.use(express.json())
 app.use(cors)
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/styles.css')
+})
 
-app.listen(6969, () => {
-    console.log('app is up on 6969')
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/main.js')
+})
+
+app.listen(4000, () => {
+    console.log('app is up on 4000')
 })
